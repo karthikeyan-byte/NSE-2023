c72dbe4a74c0ef72db9d9390da730a895c9a62c2
karthikeyan-byte <126600126+karthikeyan-byte@users.noreply.github.com>
2023-10-03 13:57:41 +0530

    Update kkk.py

--- NSE_2023/kkk.py
+++ NSE_2023/kkk.py
@@ -3,17 +3,54 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
+# Load data from a CSV file and store it in an SQLite database
 df = pd.read_csv("output.csv")
-
 conn = sqlite3.connect("app.db")
-df.to_sql("app", conn, if_exists="replace",index=False)
-
+df.to_sql("app", conn, if_exists="replace", index=False)
 conn.commit()
 conn.close()
 
+# Define the main content of your Streamlit app
+def main():
+    st.title("Streamlit App with Manage App Feature")
+    
+    # Define a sidebar section for "Manage App" actions
+    st.sidebar.header("Manage App")
+    
+    # Add options for managing the app (e.g., load data, clear data)
+    if st.sidebar.button("Load Data"):
+        load_data()
+    
+    if st.sidebar.button("Clear Data"):
+        clear_data()
+    
+    # Display data and charts in the main content area
+    df1 = eight()
+    display_data_charts(df1)
 
+# Function to load data into the app
+def load_data():
+    conn = sqlite3.connect("app.db")
+    
+    # Load data from a different CSV file (e.g., "new_data.csv") into the "app" table
+    new_data = pd.read_csv("new_data.csv")
+    new_data.to_sql("app", conn, if_exists="replace", index=False)
+    
+    conn.commit()
+    conn.close()
+
+# Function to clear data from the app
+def clear_data():
+    conn = sqlite3.connect("app.db")
+    
+    # Clear data from the "app" table by deleting all records
+    conn.execute("DELETE FROM app")
+    conn.commit()
+    
+    conn.close()
+
+# Define the "eight()" function as you did in your original code
 def eight():
-    # Connect to the SQLite database
     conn = sqlite3.connect("app.db")
 
     # Get user input for start and end dates
@@ -27,94 +64,19 @@ def eight():
     WHERE date BETWEEN ? AND ?
     """
     df1 = pd.read_sql_query(query, conn, params=(sdate, edate))
-    st.write(df1)
-    st.bar_chart(df1.set_index('date')['turnover'])
-    st.bar_chart(df1.set_index('date')['shares_traded'])
     
     conn.close()
     
-    def nin():
-        conn=sqlite3.connect("app.db")
-        df.to_sql("app", conn, if_exists="replace",index=False)
-    
-        # Calculate the sum of turnover'
-        date=df1['date'].unique()
-        total_turnover = df1['turnover'].mean()
-        total_shares_traded = df1['shares_traded'].mean()   
-
-        # Display the sum of turnover in a separate table
-        kk = pd.DataFrame({"date": [date],"Total Turnover": [total_turnover],
-                        
-                           "Total Shares Traded": [total_shares_traded]})
-        
-        kk.to_csv("fulloutput.csv", index=False)
-        conn.commit()
-        conn.close()
-        
-        return kk
-    st.write(nin())
     return df1
 
-df1=eight()
-
-def save():
-    if st.button("Load Data"):
-        df = pd.read_csv("fulloutput.csv")
-
-        conn = sqlite3.connect("channelinfo.db")
-        df.to_sql("channelinfo", conn, if_exists="append",index=False)
-
-        query="SELECT * FROM channelinfo"
-        df1 = pd.read_sql_query(query, conn)
+# Define a function to display data and charts
+def display_data_charts(df):
+    # Display data using st.write()
+    st.write(df)
     
-        conn.commit()
-        conn.close()
-    if st.button("clear"):
-        conn = sqlite3.connect("channelinfo.db")
-        conn.execute("DELETE FROM channelinfo")
-        conn.commit()
-        conn.close()
-        
-st.write(save())
-  
+    # Display charts using st.bar_chart()
+    st.bar_chart(df.set_index('date')['turnover'])
+    st.bar_chart(df.set_index('date')['shares_traded'])
 
-def one():
-    conn= sqlite3.connect("channelinfo.db")
-    query ="SELECT * FROM channelinfo;"
-    df=pd.read_sql_query(query, conn)
-    return df
-df1=one()
-st.write(df1)
-st.bar_chart(df1.set_index('date')['Total Turnover'])
-st.bar_chart(df1.set_index('date')['Total Shares Traded'])
- 
-df=pd.DataFrame([open], columns=['open'], index=[1])  
-source=pd.melt(df, id_vars=['model'])
-
-chart=alt.Chart(source).mark_bar(strokeWidth=100).encode(
-    x=alt.X('variable:N', title="", scale=alt.Scale(paddingOuter=0.5)),#paddingOuter - you can play with a space between 2 models 
-    y='value:Q',
-    color='variable:N',
-    column=alt.Column('model:N', title="", spacing =0), #spacing =0 removes space between columns, column for can and st 
-).properties( width = 300, height = 300, ).configure_header(labelOrient='bottom').configure_view(
-    strokeOpacity=0)
-
-st.altair_chart(chart) #, use_container_width=True)
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
+if __name__ == "__main__":
+    main()
